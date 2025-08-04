75270efc0a997a64c38ad63d4021f7c2d3c847f6
Liuisei <a0966891512@gmail.com>
2025-08-04 23:19:54 +0900

    UI

--- Assets/JamSeed/Script/Scene/LiuGraphite/UI/InGameUIView.cs
+++ Assets/JamSeed/Script/Scene/LiuGraphite/UI/InGameUIView.cs
@@ -1,13 +1,17 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InGameUIView : MonoBehaviour
 {
     public Slider[] _hpbar;
+
+    public TextMeshProUGUI _LVText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
              SetHPBar(InGameScene.Instance._playerDataLiu.CurrentPlayerHP); // Example: Set HP bar to full (10 HP)
+        SetLVText(InGameScene.Instance._playerDataLiu._playerLevel.ToString()); // Example: Set LV text to 1
     }
 
 
@@ -27,6 +31,11 @@ public class InGameUIView : MonoBehaviour
         {
             _hpbar[0].value = 0.5f;
         }
+    }
+
+    public void SetLVText(string lv)
+    {
+        _LVText.text = lv;
     }
 
     // Update is called once per frame
