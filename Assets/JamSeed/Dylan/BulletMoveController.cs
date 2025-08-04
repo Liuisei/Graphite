3101346076daad9a46c30f0a9e1a9fabf11df174
dylanbernard <dylanbernard@hotmail.fr>
2025-08-04 16:21:35 +0900

    Boss push

--- Assets/JamSeed/Dylan/BulletMoveController.cs
+++ Assets/JamSeed/Dylan/BulletMoveController.cs
@@ -4,6 +4,11 @@ public class BulletMoveController : MonoBehaviour
 {
     public float speed = 10f;
 
+    private void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+
     // Update is called once per frame
     void Update()
     {
